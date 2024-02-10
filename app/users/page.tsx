af8caff4b88ca2c8ad77cb05d128816af8be8831
mikegn00 <75446758+mikegn00@users.noreply.github.com>
2024-02-10 22:33:30 +0000

    Added users and product cards components

---
+++ app/users/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface User {
+  id: number;
+  name: string;
+  email : string;
+}
+
+const UsersPage = async () => {
+
+  const result = await fetch('https://jsonplaceholder.typicode.com/users', 
+  { cache: 'no-store' }
+  // { next: { revalidate: 10 } }
+  );
+
+  const users: User[] = await result.json();
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <table className='table table-borderd'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        {users.map(user => 
+        <tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+        </tr>)}
+      </table>
+    </div>
+  )
+}
+
+export default UsersPage
